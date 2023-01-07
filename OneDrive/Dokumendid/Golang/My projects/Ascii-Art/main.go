6b6edf8832177270f32c891d8daca4728c09b899
robinrattasepp <robinrattasepp18@gmail.cpm>
2023-01-08 00:20:46 +0200

    commit
    git commit -m commit

---
+++ OneDrive/Dokumendid/Golang/My projects/Ascii-Art/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+//Encoding
+func encode(input string) []int {
+	var numbers []int
+
+	for _, character := range input {
+		var number int = int(character + 0)
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+func main() {
+	//Scan the user input
+	var input string
+	fmt.Scanln(&input)
+
+	//Encode the ascii characters
+	var numbers []int = encode(input)
+
+	//-----------------------
+	//Reads the file
+	bytes, _ := ioutil.ReadFile("banner/standard.txt")
+
+	lines := strings.Split(string(bytes), "\n")
+	for i, line := range lines {
+		if i == 13 { //need to make a algorithm
+			fmt.Println(line)
+		}
+	}
+	//Encode the bytes by 8 lines
+
+	//value of fist 8 bytes is 1
+	//next 2 is 0
+	// then 8 again
+
+	//Read file
+	//assign them values.
+	// 8 long, 2 between, 8 long, 2 between
+	//each is 8 bytes long, same order as the ascii tabel
+	//replace the values
+	fmt.Println(numbers)
+	//fmt.Println(string(bytes))
+
+}
+
+// Read user input +
+//Encode +
+//Assign values to .txt files
+//to Ascii Art
