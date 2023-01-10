cecb20ce27720d02d6078e250fdbf11b994c4006
robinrattasepp <robinrattasepp18@gmail.cpm>
2023-01-10 16:16:07 +0200

    added ReadFile + err handling; Split input nd .txt

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	//Get user input
 	userInput := os.Args[1:]
 	if len(userInput) != 1 {
 		fmt.Println("Not enough arguments")
 		return
 	}
+	//Split input. Make the program accept \n as \\n
+	parts := strings.Split(strings.ReplaceAll(userInput[0], "\\n", "\n"), "\n")
 
-	fmt.Println(userInput)
+	//Read .txt fie
+	bytes, err := ioutil.ReadFile("banner/standard.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Split .txt file
+	letters := strings.Split(string(bytes), "\n")
 }
+
+//to be added: args 3, choose from shadow/standard/thinkertoy(.txt)
