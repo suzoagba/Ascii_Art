d37eb81309180142a4b561f35200db8d95c8f76f
robinrattasepp <robinrattasepp18@gmail.cpm>
2023-01-10 11:41:43 +0200

    Get user input

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
-# Ascii-Art-Web-Stylize
+# Ascii-Art
 
 ## Description
-Ascii-art consists in getting user inputcreating and running a server, in which it will be possible to use a web GUI (graphical user interface). Ascii-art-web-stylize adds designed interface.
-The webpage allows turning user input into ascii-art and offers the use of three different font styles:
-* shadow
-* standard
-* thinkertoy
 
 ## How To Run
-1) Run ```go run main.go```
-2) Add you text
-3) Press ENTER
 
 ## Implementation
-Siia kirjuta loogika.
-The server is created in Go. Ascii-art output is created from user input by looping through predefined ascii-art characters. Design is added with CSS.
+
+// Get user input
+// Split into substrings
+
+//Read .txt file
+//Split into substrings
+
+//Somehow merge them
 
 ## Authors
 Robin Rattasepp & 
 
 
- Run ```go run . "Your text" | cat -e```
--- main.go
+++ main.go
@@ -2,59 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"os"
 )
 
-//Encoding
-func encode(input string) []int {
-	var numbers []int
-
-	for _, character := range input {
-		var number int = int(character + 0)
-		numbers = append(numbers, number)
-	}
-	return numbers
-}
-
 func main() {
-	//Scan the user input
-	var input string
-	fmt.Scanln(&input)
-
-	//Encode the input
-	var numbers []int = encode(input)
-
-	//-----------------------
-	//Reads the file
-	bytes, _ := ioutil.ReadFile("banner/standard.txt")
-
-	lines := strings.Split(string(bytes), "\n")
-	x := numbers
-	beginning := (x-1)*9 + 2
-	ending := (x-1)*9 + 2 + 7
-	for i, line := range lines {
-		if i == 13 { //need to make a algorithm
-			fmt.Println(line)
-		}
+	userInput := os.Args[1:]
+	if len(userInput) != 1 {
+		fmt.Println("Not enough arguments")
+		return
 	}
-	//Encode the bytes by 8 lines
 
-	//value of fist 8 bytes is 1
-	//next 2 is 0
-	// then 8 again
-
-	//Read file
-	//assign them values.
-	// 8 long, 2 between, 8 long, 2 between
-	//each is 8 bytes long, same order as the ascii tabel
-	//replace the values
-	fmt.Println(numbers)
-	//fmt.Println(string(bytes))
-
+	fmt.Println(userInput)
 }
-
-// Read user input +
-//Encode +
-//Assign values to .txt files
-//to Ascii Art
