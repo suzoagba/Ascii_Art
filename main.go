bce8dda11360985754eaf3b5c79ec2fd680f55e7
robinrattasepp <robinrattasepp18@gmail.cpm>
2023-01-18 15:06:46 +0200

    txt file Args

--- main.go
+++ main.go
@@ -2,39 +2,67 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
-	//Check Input
-	args := os.Args[1:]
-	if len(args) != 1 {
-		fmt.Println("Not enough arguments")
+	args := os.Args
+
+	const usage = "Usage: 'go run main.go [TEXT]' or 'go run main.go [TEXT] [FONT]' "
+	if len(args) < 2 || len(args) > 3 {
+		fmt.Println(usage)
 		return
 	}
 
-	//Handle \n
-	replace := strings.ReplaceAll(args[0], "\\n", "\n")
-	//Split Input
-	parts := strings.Split(replace, "\n")
+	text := args[1]
+	font := "fonts/standard.txt"
 
-	//Read .txt File
-	bytes, err := os.ReadFile("banner/standard.txt")
-	if err != nil {
-		fmt.Println("No such file")
-		return
+	if len(args) == 3 {
+		switch args[2] {
+		case "standard", "standard.txt":
+			font = "fonts/standard.txt"
+		case "shadow", "shadow.txt":
+			font = "fonts/shadow.txt"
+		case "thinkertoy", "thinkertoy.txt":
+			font = "fonts/thinkertoy.txt"
+		default:
+			fmt.Println("Invalid font type")
+			return
+		}
 	}
 
-	//Split .txt File
-	letters := strings.Split(string(bytes), "\n")
+	if text == "\\n" {
+		fmt.Printf("\n")
+	} else if text != "" {
+		//Read .txt File
+		fontBytes, err := ioutil.ReadFile(font)
+		if err != nil {
+			fmt.Println("No such file")
+			return
+		}
 
-	for _, line := range parts {
-		for i := 1; i < 9; i++ {
-			for _, char := range line {
-				os.Stdout.WriteString(letters[(char-32)*9+rune(i)])
+		// Split .txt File
+		letters := strings.Split(string(fontBytes), "\n")
+
+		//Handle \n
+		text = strings.ReplaceAll(text, "\\n", "\n")
+
+		//Split Input
+		parts := strings.Split(text, "\n")
+
+		for _, line := range parts {
+			for i := 1; i < 9; i++ {
+				for _, char := range line {
+					os.Stdout.WriteString(letters[(char-32)*9+rune(i)])
+				}
+				if line == "" {
+					fmt.Println()
+					break
+				}
+				os.Stdout.WriteString("\n")
 			}
-			os.Stdout.WriteString("\n")
 		}
 	}
 }
